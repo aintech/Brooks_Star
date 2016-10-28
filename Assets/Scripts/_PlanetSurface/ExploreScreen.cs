3a2a6a8851bf14ffd301b6f2f509152830de615d
aintech <artfall@mail.ru>
2016-10-28 18:02:36 +0300

    Adding Scanning screen

--- Assets/Scripts/_PlanetSurface/ExploreScreen.cs
+++ Assets/Scripts/_PlanetSurface/ExploreScreen.cs
@@ -9,29 +9,32 @@ public class ExploreScreen : MonoBehaviour, ButtonHolder, Hideable {
 
 	private SpriteRenderer enemyImage;
 
-	private Button exploreBtn, fightBtn, nextBtn, backBtn, leaveBtn;
+	private Button exploreBtn, leaveBtn;//, fightBtn, nextBtn, backBtn;
 
-	private StrokeText enemyName;
+//	private StrokeText enemyName;
+//
+//	private EnemyType enemyType;
 
-	private EnemyType enemyType;
+	private ScanningScreen scanningScreen;
 
 	public ExploreScreen init (PlanetSurface planetSurface) {
 		this.planetSurface = planetSurface;
 		enemyImage = transform.Find("Enemy Image").GetComponent<SpriteRenderer>();
 
-		fightBtn = transform.Find("Fight Button").GetComponent<Button>().init();
-		nextBtn = transform.Find("Next Button").GetComponent<Button>().init();
-		backBtn = transform.Find("Back Button").GetComponent<Button>().init();
+//		fightBtn = transform.Find("Fight Button").GetComponent<Button>().init();
+//		nextBtn = transform.Find("Next Button").GetComponent<Button>().init();
+//		backBtn = transform.Find("Back Button").GetComponent<Button>().init();
 		exploreBtn = transform.Find("Explore Button").GetComponent<Button>().init();
 		leaveBtn = transform.Find("Leave Button").GetComponent<Button>().init();
 
-		enemyName = transform.Find("Enemy Name").GetComponent<StrokeText>().init("default", 2);
+//		enemyName = transform.Find("Enemy Name").GetComponent<StrokeText>().init("default", 2);
 
 		fightScreen = GameObject.Find("Fight Screen").GetComponent<FightScreen>().init(this);
+		scanningScreen = GameObject.Find("Scanning Screen").GetComponent<ScanningScreen>().init(this);
 
-		fightBtn.gameObject.SetActive(false);
-		nextBtn.gameObject.SetActive(false);
-		backBtn.gameObject.SetActive(false);
+//		fightBtn.gameObject.SetActive(false);
+//		nextBtn.gameObject.SetActive(false);
+//		backBtn.gameObject.SetActive(false);
 
 		gameObject.SetActive(false);
 
@@ -49,27 +52,27 @@ public class ExploreScreen : MonoBehaviour, ButtonHolder, Hideable {
 	}
 
 	private void startExplore () {
-		enemyName.setText("");
-		enemyName.gameObject.SetActive(true);
-		fightBtn.gameObject.SetActive(true);
-		nextBtn.gameObject.SetActive(true);
-		backBtn.gameObject.SetActive(true);
-		exploreBtn.gameObject.SetActive(false);
-		leaveBtn.gameObject.SetActive(false);
-		findEnemy();
+//		enemyName.setText("");
+//		enemyName.gameObject.SetActive(true);
+//		fightBtn.gameObject.SetActive(true);
+//		nextBtn.gameObject.SetActive(true);
+//		backBtn.gameObject.SetActive(true);
+		setVisible(false);
+		scanningScreen.show();
+//		findEnemy();
 	}
 
 	private void findEnemy () {
-		enemyType = Vars.planetType.getEnemyTypes()[Random.Range(0, Vars.planetType.getEnemyTypes().Length)];
-		enemyImage.sprite = Imager.getEnemy(enemyType, 1f);
-		enemyImage.gameObject.SetActive(true);
-		enemyName.setText(enemyType.getName());
+//		enemyType = Vars.planetType.getEnemyTypes()[Random.Range(0, Vars.planetType.getEnemyTypes().Length)];
+//		enemyImage.sprite = Imager.getEnemy(enemyType, 1f);
+//		enemyImage.gameObject.SetActive(true);
+//		enemyName.setText(enemyType.getName());
 	}
 
 	private void startFight () {
-		gameObject.SetActive(false);
-		UserInterface.showInterface = false;
-		fightScreen.startFight(enemyType);
+//		gameObject.SetActive(false);
+//		UserInterface.showInterface = false;
+//		fightScreen.startFight(enemyType);
 	}
 
 	public void endFight () {
@@ -79,29 +82,29 @@ public class ExploreScreen : MonoBehaviour, ButtonHolder, Hideable {
 	}
 
 	private void turnBack () {
-		enemyName.gameObject.SetActive(false);
-		enemyImage.gameObject.SetActive(false);
-		fightBtn.gameObject.SetActive(false);
-		nextBtn.gameObject.SetActive(false);
-		backBtn.gameObject.SetActive(false);
-		exploreBtn.gameObject.SetActive(true);
-		leaveBtn.gameObject.SetActive(true);
+//		enemyName.gameObject.SetActive(false);
+//		enemyImage.gameObject.SetActive(false);
+//		fightBtn.gameObject.SetActive(false);
+//		nextBtn.gameObject.SetActive(false);
+//		backBtn.gameObject.SetActive(false);
+//		exploreBtn.gameObject.SetActive(true);
+//		leaveBtn.gameObject.SetActive(true);
 	}
 
 	public void fireClickButton (Button btn) {
 		if (btn == exploreBtn) { startExplore(); }
-		else if (btn == fightBtn) { startFight(); }
-		else if (btn == nextBtn) { findEnemy(); }
-		else if (btn == backBtn) { turnBack(); }
+//		else if (btn == fightBtn) { startFight(); }
+//		else if (btn == nextBtn) { findEnemy(); }
+//		else if (btn == backBtn) { turnBack(); }
 		else if (btn == leaveBtn) { leavePlanet(); }
 		else { Debug.Log("Unknown button: " + btn.name); }
 	}
 
 	public void setVisible (bool visible) {
 		exploreBtn.setVisible(visible);
-		fightBtn.setVisible(visible);
-		nextBtn.setVisible(visible);
-		backBtn.setVisible(visible);
+//		fightBtn.setVisible(visible);
+//		nextBtn.setVisible(visible);
+//		backBtn.setVisible(visible);
 		leaveBtn.setVisible(visible);
 	}
 }
---
+++ Assets/Scripts/_PlanetSurface/ScanningScreen.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScanningScreen : MonoBehaviour, ButtonHolder {
+
+	private ExploreScreen exploreScreen;
+
+	private Button closeBtn;
+
+	private GameObject[] barBlocks;
+
+	private Transform cursor;
+
+	private Vector2 cursorPos;
+
+	private List<Vector2> targets = new List<Vector2>();
+
+	private float ableDistance = 1, distance, tempFloat;
+
+	public ScanningScreen init (ExploreScreen exploreScreen) {
+		this.exploreScreen = exploreScreen;
+
+		cursor = transform.Find("Cursor").transform;
+		cursor.gameObject.SetActive(true);
+		closeBtn = transform.Find("Close Button").GetComponent<Button>().init();
+		transform.Find("Background").gameObject.SetActive(true);
+
+
+		Transform barsHolder = transform.Find("Bars Holder");
+		barBlocks = new GameObject[barsHolder.childCount];
+		char[] separ = new char[]{' '};
+		int pos;
+		for (int i = 0; i < barsHolder.childCount; i++) {
+			pos = int.Parse(barsHolder.GetChild(i).name.Split(separ, int.MaxValue)[2]);
+			barBlocks[i] = barsHolder.GetChild(i).gameObject;
+		}
+		barsHolder.gameObject.SetActive(true);
+
+		close();
+		return this;
+	}
+
+	void Update () {
+		cursor.localPosition = Utils.mousePos;
+		findNearestTarget();
+	}
+
+	private void addTargets () {
+		targets.Clear();
+		targets.Add(Vector2.zero);
+	}
+
+	private void findNearestTarget () {
+		distance = 1000000;
+		foreach (Vector2 targ in targets) {
+			tempFloat = Vector2.Distance(targ, Utils.mousePos);
+			if (tempFloat < distance) { distance = tempFloat; }
+		}
+		arrangeBlocks(distance < ableDistance?  (distance / ableDistance): 0);
+	}
+
+	private void arrangeBlocks (float value) {
+		if (value < .01f) {
+			for (int i = 0; i < barBlocks.Length; i++) {
+				if (barBlocks[i].activeInHierarchy) { barBlocks[i].SetActive(false); }
+			}
+		} else {
+			int activeBlocks = barBlocks.Length - Mathf.RoundToInt(value * barBlocks.Length);
+			for (int i = 0; i < barBlocks.Length; i++) {
+				barBlocks[i].SetActive(i <= activeBlocks);
+			}
+		}
+	}
+
+	public void show () {
+		gameObject.SetActive(true);
+		UserInterface.showInterface = false;
+		addTargets();
+	}
+
+	public void close () {
+		gameObject.SetActive(false);
+		exploreScreen.setVisible(true);
+		UserInterface.showInterface = true;
+	}
+
+	public void fireClickButton (Button btn) {
+		if (btn == closeBtn) { close(); }
+	}
+}
